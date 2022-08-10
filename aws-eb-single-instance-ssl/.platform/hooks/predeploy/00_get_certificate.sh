7472d1b8be308330f9b364f1a98895dc317a17c3
Binh Bui <binhbv@gmail.com>
2022-08-10 19:43:32 +0700

    xxx

--- aws-eb-single-instance-ssl/.platform/hooks/predeploy/00_get_certificate.sh
+++ aws-eb-single-instance-ssl/.platform/hooks/predeploy/00_get_certificate.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 # Change CERTBOT_DOMAINS to your domain and CERTBOT_EMAIL to your email address
 sudo certbot -n -d ${CERTBOT_DOMAINS} --nginx --agree-tos --email ${CERTBOT_EMAIL}
+ln -sf /etc/letsencrypt/live/${CERTBOT_DOMAINS} /etc/letsencrypt/live/ebcert
