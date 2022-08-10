3500a817815e29fa1f8df0db57209b5531580b70
Binh Bui <binhbv@gmail.com>
2022-08-10 14:42:13 +0700

    certbot

---
+++ aws-eb-single-instance-ssl/index.js
@@ -0,0 +1,12 @@
+import { createServer } from 'http';
+const port = process.env.PORT || 8080;
+
+const server = createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+});
+
+// Listen on port 8080, IP defaults to 127.0.0.1
+server.listen(port);
+
+console.log('Server running at http://127.0.0.1:' + port + '/');
