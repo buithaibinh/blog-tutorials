8acc4eef68e4b404eab04c6647874a22878d1b8a
Binh Bui <binhbv@gmail.com>
2022-08-10 17:18:09 +0700

    xx

--- aws-eb-single-instance-ssl/.platform/hooks/predeploy/00_get_certificate.sh
+++ aws-eb-single-instance-ssl/.platform/hooks/predeploy/00_get_certificate.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Change DOMAIN to your domain and EMAIL to your email address
-sudo certbot -n -d test-ssl.sk-global.io --nginx --agree-tos --email bvbinh+ssl1@sk-global.biz
-ln -sf /etc/letsencrypt/live/test-ssl.sk-global.io /etc/letsencrypt/live/ebcert
+sudo certbot -n -d demo-ssl.sk-global.io --nginx --agree-tos --email bvbinh+ssl1@sk-global.biz
+ln -sf /etc/letsencrypt/live/demo-ssl.sk-global.io /etc/letsencrypt/live/ebcert
