473e8455d9282bb1b3649ffdca9784f10f8e69b9
Binh Bui <binhbv@gmail.com>
2022-08-10 18:09:49 +0700

    xxx

--- aws-eb-single-instance-ssl/.platform/hooks/postdeploy/00_get_certificate.sh
+++ aws-eb-single-instance-ssl/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-# Change DOMAIN to your domain and EMAIL to your email address
+# Change CERTBOT_DOMAINS to your domain and CERTBOT_EMAIL to your email address
 sudo certbot -n -d ${CERTBOT_DOMAINS} --nginx --agree-tos --email ${CERTBOT_EMAIL}
