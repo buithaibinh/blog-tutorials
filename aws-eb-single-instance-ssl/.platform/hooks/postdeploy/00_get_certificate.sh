4144866a0bcccd06be3a632876688a2e97325016
Binh Bui <binhbv@gmail.com>
2022-08-10 16:02:05 +0700

    xxx

--- aws-eb-single-instance-ssl/.platform/hooks/postdeploy/00_get_certificate.sh
+++ aws-eb-single-instance-ssl/.platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+# Change DOMAIN to your domain and EMAIL to your email address
 sudo certbot -n -d test-ssl.sk-global.io --nginx --agree-tos --email bvbinh+ssl1@sk-global.biz
-# sudo chown ec2-user -R /etc/letsencrypt
+ln -sf /etc/letsencrypt/live/test-ssl.sk-global.io /etc/letsencrypt/live/ebcert
