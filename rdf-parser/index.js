fe1c8b8d67589edd5a140934abd03b01f549dba0
Binh Bui <binhbv@gmail.com>
2022-08-15 16:23:17 +0700

    rdf-parser

---
+++ rdf-parser/index.js
@@ -0,0 +1,20 @@
+const cheerio = require('cheerio');
+const { readFileSync } = require('fs');
+
+(() => {
+  const data = readFileSync('./rdf.xml', 'utf8');
+
+  // create a cheerio instance
+  const $ = cheerio.load(data);
+  let collect = function (index, el) {
+    const item = {};
+    const url = $(el).attr('rdf:about');
+    item.title = $(el).find('title').text();
+    item.link = url;
+    item.pubDate = $(el).find('dc\\:date').text();
+    return item;
+  };
+
+  const items = $('item').map(collect).get();
+  console.log(items);
+})();
