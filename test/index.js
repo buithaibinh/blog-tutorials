56d402a5c56fb5cd927541734e2788780d520c8e
Binh Bui <binhbv@gmail.com>
2022-10-04 14:59:40 +0700

    npx-sample

---
+++ test/index.js
@@ -0,0 +1,13 @@
+const {
+  fromUrl,
+  fromBufferWithMimeType,
+  fromBuffer,
+} = require('@sk-global/text-extractor');
+
+(async () => {
+  console.log('Hello World!');
+  const text = await fromUrl(
+    'https://www.digital.go.jp/assets/contents/node/basic_page/field_ref_resources/d6cfdcdd-75e4-460c-9ec0-af4f952e03d5/20210906_meeting_promoting_01.pdf'
+  );
+  console.log(text);
+})();
