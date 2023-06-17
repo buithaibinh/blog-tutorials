4609ff3451c390a476dd2a88cb0d06dec1de4e4e
Binh Bui <binhbv@gmail.com>
2023-06-17 15:55:15 +0700

    x

---
+++ test/es.mjs
@@ -0,0 +1,39 @@
+import { Client } from '@elastic/elasticsearch';
+import fs from 'fs';
+import dotenv from 'dotenv';
+dotenv.config({
+  path: '.env'
+});
+
+const client = new Client({
+  cloud: {
+    id: process.env.ELASTIC_CLOUD_ID
+  },
+  auth: {
+    apiKey: {
+      id: process.env.ELASTIC_API_KEY_ID,
+      api_key: process.env.ELASTIC_API_KEY
+    }
+  }
+});
+
+const main = async () => {
+  // load 100 videos from the database
+  const data = await client.search({
+    index: 'videos',
+    body: {
+      query: {
+        match_all: {}
+      }
+    },
+    size: 100,
+    _source: ['title', 'description', 'id']
+  });
+  const videos = data.hits.hits.map((hit) => {
+    return hit._source;
+  });
+
+  fs.writeFileSync('videos.json', JSON.stringify(videos, null, 2));
+};
+
+main();
--- test/rc9.mjs
+++ test/rc9.mjs
@@ -1,10 +1,43 @@
-import { writeUser, readUser } from 'rc9';
+import { DATA } from './videos.mjs';
+
+import fs from 'fs';
+
+const randomNum = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
 
 const main = async () => {
-  writeUser({ token: 123 }, '.zoorc'); // Will be saved in {home}/.zoorc
+  // convert array to object
+  const videoStats = {};
+  const keywordStats = {};
+  DATA.forEach((video) => {
+    videoStats[video.id] = {
+      LIKE: randomNum(0, 100),
+      VIEW: randomNum(0, 100),
+      PIN: randomNum(0, 100),
+      NOT_INTERESTS: randomNum(0, 100)
+    };
+    const keywords = video.tags.split(',').slice(0, 5);
+    keywords.forEach((keyword) => {
+      keywordStats[keyword] = {
+        point: keywordStats[keyword] ? keywordStats[keyword].point + 1 : 1,
+        LIKE: randomNum(0, 100),
+        VIEW: randomNum(0, 100),
+        PIN: randomNum(0, 100),
+        NOT_INTERESTS: randomNum(0, 100)
+      };
+    });
+  });
 
-  const conf = readUser('.zoorc'); // { token: 123 }
-  console.log(conf.token); // 123
+  const stats = {
+    id: 'STATS-2023-5-17',
+    videoStats,
+    keywordStats
+  };
+  // save to file
+  fs.writeFileSync('stats.json', JSON.stringify(stats, null, 2));
+
+  // build stats
 };
 
 main();
